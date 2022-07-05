696b0bfae005e8d63a7ae58e71970b6feb26b9b6
fajarsid <fajarsuganda79@gmail.com>
2022-07-05 10:07:24 +0700

    update crud

--- src/components/Maps/Mapbox.jsx
+++ src/components/Maps/Mapbox.jsx
@@ -1,58 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import ReactMapGL, {
-  FullscreenControl,
-  NavigationControl,
-  Source,
-  Layer,
-} from 'react-map-gl';
-import {
-  Button,
-  Card,
-  CardContent,
-  CardActions,
-  Box,
-  Typography,
-  Table,
-  TableCell,
-  TableContainer,
-  TableRow,
-  TableHead,
-  Paper,
-  TableBody
-} from '@mui/material';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import mapboxgl from 'mapbox-gl';
-import axios from 'axios';
-import * as turf from '@turf/turf';
+import React, { useEffect, useState } from "react";
+import ReactMapGL, { FullscreenControl, NavigationControl, Source, Layer } from "react-map-gl";
+import { Button, Card, CardContent, CardActions, Box, Typography, Table, TableCell, TableContainer, TableRow, TableHead, Paper, TableBody } from "@mui/material";
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
+import axios from "axios";
+import * as turf from "@turf/turf";
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
+import MapboxWorker from "worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker";
 
 mapboxgl.workerClass = MapboxWorker;
 
 const layerLines = {
-  id: 'lines',
-  type: 'line',
-  source: 'geojson-lines',
+  id: "lines",
+  type: "line",
+  source: "geojson-lines",
   layout: {
-    'line-cap': 'round',
-    'line-join': 'round',
+    "line-cap": "round",
+    "line-join": "round",
   },
   paint: {
-    'line-color': ['get', 'color'],
-    'line-width': 8,
+    "line-color": ["get", "color"],
+    "line-width": 8,
   },
-  filter: ['in', '$type', 'LineString'],
+  filter: ["in", "$type", "LineString"],
 };
 
 const layerSymbols = {
-  id: 'symbols',
-  type: 'symbol',
-  source: 'route',
+  id: "symbols",
+  type: "symbol",
+  source: "route",
   layout: {
-    'symbol-placement': 'line',
-    'text-font': ['Open Sans Regular'],
-    'text-field': '{title}', // part 2 of this is how to do it
-    'text-size': 16,
+    "symbol-placement": "line",
+    "text-font": ["Open Sans Regular"],
+    "text-field": "{title}", // part 2 of this is how to do it
+    "text-size": 16,
   },
 };
 
@@ -68,27 +49,20 @@ const geolocateControlStyle = {
 
 const CardDetail = ({ data }) => {
   return (
-    <Box
-      sx={{ minWidth: 325 }}
-      style={{ position: 'absolute', top: 10, left: 20, zIndex: 104 }}
-    >
+    <Box sx={{ minWidth: 325 }} style={{ position: "absolute", top: 10, left: 20, zIndex: 104 }}>
       <Card>
         <CardContent>
-          <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
             Daftar Jalur Evakuasi
           </Typography>
 
           <TableContainer component={Paper}>
-            <Table
-              sx={{ minWidth: 300 }}
-              size='small'
-              aria-label='a dense table'
-            >
+            <Table sx={{ minWidth: 300 }} size="small" aria-label="a dense table">
               <TableHead>
                 <TableRow>
                   <TableCell>Nama</TableCell>
-                  <TableCell align='right'>Warna</TableCell>
-                  <TableCell align='right'>Jarak (Km)</TableCell>
+                  <TableCell align="right">Warna</TableCell>
+                  <TableCell align="right">Jarak (Km)</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -98,16 +72,17 @@ const CardDetail = ({ data }) => {
                     return (
                       <TableRow
                         sx={{
-                          '&:last-child td, &:last-child th': { border: 0 },
+                          "&:last-child td, &:last-child th": { border: 0 },
                         }}
                       >
-                        <TableCell component='th' scope='row'>
+                        <TableCell component="th" scope="row">
                           {d.properties.title}
                         </TableCell>
-                        <TableCell align='right'>
-                          {d.properties.color}
-                        </TableCell>
-                        <TableCell align='right'>{distance.toLocaleString()}</TableCell>
+                        <TableCell align="right">{d.properties.color}</TableCell>
+                        <TableCell align="right">{distance.toLocaleString()}</TableCell>
+                        <Button variant="contained" style={{ float: "center", textTransform: "capitalize", fontSize: "10px" }}>
+                          Hapus
+                        </Button>
                       </TableRow>
                     );
                   })}
@@ -120,21 +95,17 @@ const CardDetail = ({ data }) => {
   );
 };
 
-const MapBox = ({
-  children,
-  mapStyle = 'mapbox://styles/mapbox/streets-v11',
-  height,
-}) => {
+const MapBox = ({ children, mapStyle = "mapbox://styles/mapbox/streets-v11", height }) => {
   const [viewport, setViewport] = useState({
     height: 500,
-    width: '100%',
+    width: "100%",
     latitude: -6.8547344868726166,
     longitude: 106.97880824990715,
     zoom: 14.8,
   });
 
   const [geoJson, setGeoJson] = useState({
-    type: 'FeatureCollection',
+    type: "FeatureCollection",
     features: [],
   });
 
@@ -145,11 +116,11 @@ const MapBox = ({
   const getRutes = async () => {
     try {
       const newFeatures = [];
-      const url = 'http://localhost:8080/api/lines/';
+      const url = "http://localhost:8080/api/lines/";
       const { data } = await axios.get(url);
       data.map((d) => {
         newFeatures.push({
-          type: 'Feature',
+          type: "Feature",
           properties: {
             color: d.warna,
             title: d.nama,
@@ -171,9 +142,7 @@ const MapBox = ({
     <ReactMapGL
       {...viewport}
       height={height}
-      mapboxAccessToken={
-        'pk.eyJ1Ijoidm9uaXNzYWMiLCJhIjoiY2w1MHVqZzlvMDh0bjNsb2hyZTBoenE1aSJ9.j-_W1GMSLdpsBj7MJ9_JhA'
-      }
+      mapboxAccessToken={"pk.eyJ1Ijoidm9uaXNzYWMiLCJhIjoiY2w1MHVqZzlvMDh0bjNsb2hyZTBoenE1aSJ9.j-_W1GMSLdpsBj7MJ9_JhA"}
       mapStyle={mapStyle}
       onDrag={(e) =>
         setViewport({
@@ -186,7 +155,7 @@ const MapBox = ({
       onViewportChange={(nextViewport) => setViewport(nextViewport)}
     >
       {geoJson.features.length > 0 && (
-        <Source id='geojson-lines' type='geojson' data={geoJson}>
+        <Source id="geojson-lines" type="geojson" data={geoJson}>
           <Layer {...layerLines} />
           <Layer {...layerSymbols} />
         </Source>
